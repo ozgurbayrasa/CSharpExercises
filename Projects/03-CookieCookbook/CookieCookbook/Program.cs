d0324b2f1a972f3262b7c33c4d2ac6605c8bddcb
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-10 01:19:50 +0300

    High-Level Design

--- Projects/03-CookieCookbook/CookieCookbook/Program.cs
+++ Projects/03-CookieCookbook/CookieCookbook/Program.cs
@@ -1,3 +1,52 @@
-﻿using CookieCookbook.App;
+﻿
+using CookieCookbook.App;
 
-Main.ExecuteCookieCookbookApp();
+var cookieRecipesApp = new CookieRecipesApp();
+cookieRecipesApp.Run();
+
+public class CookieRecipesApp
+{
+
+    private readonly RecipesRepository _recipesRepository;
+    private readonly RecipesUserInteraction _recipesUserInteraction;
+
+    public CookieRecipesApp(
+        RecipesRepository recipesRepository,
+        RecipesUserInteraction recipesUserInteraction)
+    {
+        _recipesRepository = recipesRepository;
+        _recipesUserInteraction = recipesUserInteraction;
+    }
+
+    // Responsibility => Managing Workflow of this app.
+    public void Run()
+    {
+        // Reading all the recipes.
+        var allRecipes = _recipesRepository.Read(filePath);
+        _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+
+        _recipesUserInteraction.PromptToCreateRecipe();
+
+        var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
+
+        if(ingredients.Count > 0)
+        {
+            var recipe = new Recipe(ingredients);
+            allRecipes.Add(recipe);
+            _recipesRepository.Write(filePath, allRecipes);
+
+            _recipesUserInteraction.ShowMessage("Recipe added: ");
+            _recipesUserInteraction.ShowMessage(recipe.ToString());
+        }
+        else
+        {
+            _recipesUserInteraction.ShowMessage(
+                "No ingredients have been selected. " +
+                "Recipe will not be saved.");
+        }
+
+        _recipesUserInteraction.Exit();
+
+    }
+}
+
