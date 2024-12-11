b1c2686ac294ce2002b1c693485a18f13f5ccdbd
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-11 03:35:20 +0300

    Exception Rethrowing Exercise Implementation

---
+++ Exercises/02-ExceptionHandling/ExceptionHandling/02-ExceptionRethrowing.cs
@@ -0,0 +1,22 @@
+﻿namespace ExceptionHandling
+{
+    public static class ExceptionsRethrowing
+    {
+        public static int GetMaxValue(List<int> numbers)
+        {
+            try
+            {
+                return numbers.Max();
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new ArgumentNullException("The numbers list cannot be null.", ex);
+            }
+            catch(InvalidOperationException ex)
+            {
+                Console.WriteLine("The numbers list cannot be empty.");
+                throw;
+            }
+        }
+    }
+}
