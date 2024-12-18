960f2ababd1bdcc448fd167083b0cbe881d5e8ec
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-18 22:45:35 +0300

    Implementing Integer List class

--- Exercises/03-GenericTypesAndAdvancedMethods/GenericTypesAndAdvancedMethods/Program.cs
+++ Exercises/03-GenericTypesAndAdvancedMethods/GenericTypesAndAdvancedMethods/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Implementation of int array.
+class ListOfInts
+{
+    // Initial size is 4. (For example)
+    // In real list it is 0.
+    private int[] _items = new int[4];
+    // Size is 0 by default.
+    private int _size = 0;
+
+    // Adds an integer to the integer list.
+    public void Add(int item)
+    { 
+        // If there is no room for new item, double the size
+        // of the list.
+        if(_size >= _items.Length)
+        {
+            var newItems = new int[_items.Length * 2];
+
+            for(int i = 0; i < _items.Length; i++)
+            {
+                newItems[i] = _items[i];
+            }
+            _items = newItems;
+        }
+
+        // Add the item and increment the size.
+        _items[_size] = item;
+        ++_size;
+    }
+}
+
+
+Console.ReadKey();
