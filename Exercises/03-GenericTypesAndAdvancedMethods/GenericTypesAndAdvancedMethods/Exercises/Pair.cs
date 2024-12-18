7e635d96f8326d2c7ea791936fa8cf768e4bac66
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-19 01:19:41 +0300

    Pair Exercise Implemented

---
+++ Exercises/03-GenericTypesAndAdvancedMethods/GenericTypesAndAdvancedMethods/Exercises/Pair.cs
@@ -0,0 +1,24 @@
+﻿namespace GenericTypesAndAdvancedMethods.Exercises
+{
+    public class Pair<T>
+    {
+        public T First { get; private set; }
+        public T Second { get; private set; }
+
+        public Pair(T first, T second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public void ResetFirst()
+        {
+            First = default;
+        }
+
+        public void ResetSecond()
+        {
+            Second = default;
+        }
+    }
+}
