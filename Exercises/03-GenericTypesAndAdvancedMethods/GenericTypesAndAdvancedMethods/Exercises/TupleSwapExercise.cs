802f1386e17927b32347094de2bb5513f551a5b2
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-20 02:33:22 +0300

    TupleSwapExercise Implented

---
+++ Exercises/03-GenericTypesAndAdvancedMethods/GenericTypesAndAdvancedMethods/Exercises/TupleSwapExercise.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypesAndAdvancedMethods.Exercises
+{
+    public static class TupleSwapExercise
+    {
+        public static Tuple<T2, T1> SwapTupleItems<T1, T2>
+            (Tuple<T1, T2> inputTuple)
+        {
+            return new Tuple<T2, T1>(inputTuple.Item2, inputTuple.Item1);
+        }
+    }
+}
