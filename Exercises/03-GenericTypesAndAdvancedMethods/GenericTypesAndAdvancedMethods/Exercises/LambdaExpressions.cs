71d6434c5fa8a2bb372cbed2992ba0bfc413b727
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-22 01:18:39 +0300

    Lambda Expressions Exercise

---
+++ Exercises/03-GenericTypesAndAdvancedMethods/GenericTypesAndAdvancedMethods/Exercises/LambdaExpressions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypesAndAdvancedMethods.Exercises
+{
+    public class ExerciseLambda
+    {
+        public Func<string, int> GetLength = word => word.Length;
+        public Func<int> GetRandomNumberBetween1And10 = () => new Random().Next(1, 11);
+    }
+}
