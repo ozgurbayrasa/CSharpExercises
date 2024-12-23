2d1d9092f44e4c0692150d015287a9ee41c7ec1f
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-24 00:40:46 +0300

    Linq OrderBy and First-Last Example

---
+++ Projects/04-Linq/Linq/LinqExercises/OrderByAndFirstLast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.LinqExercises
+{
+    internal class OrderByAndFirstLast
+    {
+        public static string FindShortestWord(List<string> words)
+        {
+            // Shortest word returned.
+            return words.OrderByDescending(word => word.Length).Last();
+        }
+    }
+}
