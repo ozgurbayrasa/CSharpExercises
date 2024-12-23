479f75c2685dd1c910f7b1c8770b7a1341e0eaf9
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-23 13:23:24 +0300

    Dictionaries Example Implemented

---
+++ Exercises/03-GenericTypesAndAdvancedMethods/GenericTypesAndAdvancedMethods/Exercises/Dictionaries.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypesAndAdvancedMethods.Exercises
+{
+    public static class ExerciseDictionary
+    {
+        public static Dictionary<PetType, double> FindMaxWeights(List<Pet> pets)
+        {
+            
+            Dictionary<PetType, double> maximumWeightByPetType = new Dictionary<PetType, double>();
+            double maximumWeight = default;
+
+            foreach (Pet pet in pets)
+            {
+                if(!maximumWeightByPetType.ContainsKey(pet.PetType))
+                {
+                    maximumWeightByPetType.Add(pet.PetType, pet.Weight);
+                    maximumWeight = pet.Weight;
+                    
+                }
+                else if (maximumWeightByPetType[pet.PetType] > maximumWeight)
+                {
+                    maximumWeightByPetType[pet.PetType] = pet.Weight;
+                    maximumWeight = pet.Weight;
+                }
+            }
+
+            return maximumWeightByPetType;
+        }
+    }
+
+    public class Pet
+    {
+        public PetType PetType { get; }
+        public double Weight { get; }
+
+        public Pet(PetType petType, double weight)
+        {
+            PetType = petType;
+            Weight = weight;
+        }
+
+        public override string ToString() => $"{PetType}, {Weight} kilos";
+    }
+
+    public enum PetType { Dog, Cat, Fish }
+}
