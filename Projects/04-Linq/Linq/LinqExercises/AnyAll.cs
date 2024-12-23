cfd35803d7aebbd1aecdf7653a683a2247bf6908
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-24 00:40:21 +0300

    Linq AnyAll Example

---
+++ Projects/04-Linq/Linq/LinqExercises/AnyAll.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Linq.LinqExercises
+{
+    public class LinqExercise
+    {
+        public static bool IsAnyWordWhiteSpace(List<string> words)
+        {
+            return words.Any(word => word.All((letter) => char.IsWhiteSpace(letter)));
+        }
+    }
+}
