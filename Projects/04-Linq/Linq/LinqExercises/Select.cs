f8d25a60ee42012fa07690e03ec3c54a73e3b113
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-24 01:47:31 +0300

    Select Exercise Impelemented

---
+++ Projects/04-Linq/Linq/LinqExercises/Select.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.LinqExercises
+{
+    internal class Select
+    {
+        public static double CalculateAverageDurationInMilliseconds(IEnumerable<TimeSpan> timeSpans)
+        {
+            return timeSpans.Select(timeSpan => timeSpan.TotalMilliseconds).Average();
+        }
+    }
+}
