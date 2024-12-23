d784b64b41ac75121ea0aacad98f8093a019c543
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-23 19:32:12 +0300

    Extension Methods Linq

---
+++ Projects/04-Linq/Linq/Program.cs
@@ -0,0 +1,11 @@
+﻿// Linq on string list.
+var words = new List<string> { "aa", "b", "ccc"};
+var wordsLongerThan2Letters = words.Where((word) => word.Length > 2);
+
+// Linq on integere array.
+var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+var oddNumbers = numbers.Where(number => number %  2 == 1);
+
+
+
+Console.ReadKey();
