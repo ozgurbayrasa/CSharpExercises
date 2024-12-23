b5e703c0599aec727903cacd6924d9fa186c31ed
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-23 20:09:14 +0300

    Method Chaining

--- Projects/04-Linq/Linq/Program.cs
+++ Projects/04-Linq/Linq/Program.cs
@@ -2,10 +2,17 @@
 var words = new List<string> { "aa", "b", "ccc"};
 var wordsLongerThan2Letters = words.Where((word) => word.Length > 2);
 
-// Linq on integere array.
+// Linq on integers array.
 var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7 };
 var oddNumbers = numbers.Where(number => number %  2 == 1);
 
+// Original IEnumerable is not modified.
+var numbersAppend = numbers.Append(8);
+
+// Method Chaining.
+var orderedOddNumbers = numbers
+    .Where(number => number % 2 == 1)
+    .OrderBy(number => number);
 
 
 Console.ReadKey();
