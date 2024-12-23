806ef5a3b44f9f7423c64dc3d608908964621055
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-24 00:40:30 +0300

    Linq Count Example

---
+++ Projects/04-Linq/Linq/LinqExercises/Count.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq.LinqExercises
+{
+    public class LinqExercise2
+    {
+        public static int CountListsContainingZeroLongerThan(
+             int length,
+             List<List<int>> listsOfNumbers)
+        {
+            return listsOfNumbers.Count(
+                intList => intList.Count() > length && 
+                intList.Contains(0));
+        }
+    }
+}
