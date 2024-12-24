6131e24b2f1591b69d0fb29680fb116cc72c791a
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-25 02:19:58 +0300

    Ref Mofdifier Example

---
+++ Exercises/05-DotNet/DotNet/ExercisesDotNet/RefModifier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.ExercisesDotNet
+{
+    public class RefModifier
+    {
+        public static void FastForwardToSummer(ref DateTime inputDate)
+        {
+            var firstDayOfSummer = new DateTime(inputDate.Year, 6, 21);
+
+            if (firstDayOfSummer < inputDate) return;
+
+            inputDate = firstDayOfSummer;
+
+        }
+    }
+}
