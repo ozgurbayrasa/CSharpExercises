669e433e0fe7ebceba0c073cfa425b839f6976f4
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-25 02:42:52 +0300

    Reference Keyword for Reference Type

--- Exercises/05-DotNet/DotNet/Program.cs
+++ Exercises/05-DotNet/DotNet/Program.cs
@@ -1,23 +1,56 @@
 ﻿
-
-
-int number = 5;
 var John = new Person { Name = "John", Age = 34 };
 
-Console.WriteLine("Number before: " + number); // 5
-Console.WriteLine("John's Age before: " + John.Age); // 34
-
-AddOneToNumber(number);
 AddOneAgeToPerson(John);
 
-Console.WriteLine("Number now: " + number); // 5 (Same)
-Console.WriteLine("John's Age now: " + John.Age); // 35 (Changed)
+int number = 5;
+Console.WriteLine("Number before: " + number); // 5
+
+// It should be passed as reference.
+AddOneToNumber(ref number);
+
+Console.WriteLine("Number after: " + number); // 6
+
+
+int numberNotInitialized;
+// Number definetely will be assigned to a value in method.
+AddOneToNumberOut(out numberNotInitialized);
+Console.WriteLine("numberNotInitialized: " +  numberNotInitialized); // 10
+
+// Ref Modifier with Reference Type
+
+// List is initialized (reference type)
+var list = new List<int> {1,2,3};
+
+// Set list to null inside the method.
+SetListToNull(ref list);
+
+// List is null.
+Console.WriteLine($"List is {((list is null) ? "null" : "not null")}");
+
+
+// 
+
+void SetListToNull(ref List<int> list)
+{
+    list = null;
+}
 
 Console.ReadLine();
 
-void AddOneToNumber(int number)
+// Making value type paramater to reference type
+// So original value will be effected.
+void AddOneToNumber(ref int number)
 {
     number++;
+}
+
+// Not initialized paramater can be passed here.
+// It will be returned as 10 even if this method
+// is void method.
+void AddOneToNumberOut(out int number)
+{
+    number = 10;
 }
 
 void AddOneAgeToPerson(Person person)
