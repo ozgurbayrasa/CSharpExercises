b7d40bf0ccb028b7b37e24f18a12893381743541
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-25 00:49:08 +0300

    Refence and Value Type

---
+++ Exercises/05-DotNet/DotNet/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+int number = 5;
+var John = new Person { Name = "John", Age = 34 };
+
+Console.WriteLine("Number before: " + number); // 5
+Console.WriteLine("John's Age before: " + John.Age); // 34
+
+AddOneToNumber(number);
+AddOneAgeToPerson(John);
+
+Console.WriteLine("Number now: " + number); // 5 (Same)
+Console.WriteLine("John's Age now: " + John.Age); // 35 (Changed)
+
+Console.ReadLine();
+
+void AddOneToNumber(int number)
+{
+    number++;
+}
+
+void AddOneAgeToPerson(Person person)
+{
+    person.Age++;
+}
+
+internal class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
