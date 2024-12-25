0f5372b19102a2f2d3ae7587c3e2ba7e1ba32083
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-25 03:24:55 +0300

    Boxing and Unboxing

--- Exercises/05-DotNet/DotNet/Program.cs
+++ Exercises/05-DotNet/DotNet/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Collections;
+
 var John = new Person { Name = "John", Age = 34 };
 
 AddOneAgeToPerson(John);
@@ -25,16 +27,67 @@ var list = new List<int> {1,2,3};
 // Set list to null inside the method.
 SetListToNull(ref list);
 
-// List is null.
-Console.WriteLine($"List is {((list is null) ? "null" : "not null")}");
-
-
-// 
-
 void SetListToNull(ref List<int> list)
 {
     list = null;
 }
+
+// List is null.
+Console.WriteLine($"List is {((list is null) ? "null" : "not null")}");
+
+
+// Unified Type System
+
+var variousObjects = new List<object>
+{
+    // Boxing happens implicitly.
+    // Value-types assigned to list of 'object' instances.
+    1, // Boxing
+    1.5m, // Boxing
+    new DateTime(2024,4,5), // Boxing
+    "hello",
+    new Person { Name = "Anna", Age = 35}
+};
+
+foreach (var someObject in variousObjects)
+{
+    // Some object is reference type.
+    // But there is some int, decimal and DateTime objects.
+    // They are value types... (boxing)...
+    Console.WriteLine(someObject + " type is: " + someObject.GetType().Name);
+}
+
+// Boxing - Unboxing
+
+int number5 = 5; // in Stack
+
+// reference in Stack, person object in Heap
+var person = new Person { Name = "Ted", Age = 19 };
+
+// Assigning value type to a variable of reference type.
+// Value is boxed [new object -> 5] 
+// Stack -> reference to Boxed Number
+// Heap -> object(5)
+object boxedNumber = number5;
+
+// Boxing happend implicitly each time we assign a value type
+// to an instance of reference type.
+
+// Unboxing explicilty
+int unboxedNumber = (int)boxedNumber;
+
+// boxedNumber was int not short. Compiler cannot understand it.
+// Run-time error raises.
+short unboxedNumberNotExplicilty = (short) boxedNumber;
+
+// No boxing -> Performance and memory efficient.
+var numbersList = new List<int> { 1, 2, 3 };
+
+// Boxing -> Not efficiently for performance and memory.
+var numbersArrayList = new ArrayList { 1, 2, 3 };
+
+// Boxing -> List is a list of reference types -> Be Careful.
+var numbersListI = new List<IComparable<int>> { 1, 2, 3 };
 
 Console.ReadLine();
 
