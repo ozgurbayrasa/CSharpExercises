f9fc3ce32890ba8292b58510d577d3f94cc54855
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-26 06:23:22 +0300

    Dispose method exercise.

---
+++ Exercises/05-DotNet/DotNet/ExercisesDotNet/AllLinesFromTextFileReader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.ExercisesDotNet
+{
+    public class AllLinesFromTextFileReader : IDisposable 
+    {
+        private readonly StreamReader _streamReader;
+
+        public AllLinesFromTextFileReader(string filePath)
+        {
+            _streamReader =  new StreamReader(filePath);
+        }
+
+        public List<string> ReadAllLines()
+        {
+            var result = new List<string>();
+            while (!_streamReader.EndOfStream)
+            {
+                result.Add(_streamReader.ReadLine());
+            }
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            _streamReader.Dispose();
+        }
+    }
+}
