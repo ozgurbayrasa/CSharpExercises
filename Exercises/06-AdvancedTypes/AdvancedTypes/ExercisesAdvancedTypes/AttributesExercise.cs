036eebf0ee04e935fcfab8ef96501bf7d0198a15
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-30 02:24:33 +0300

    Removing unnecessary includes.

--- Exercises/06-AdvancedTypes/AdvancedTypes/ExercisesAdvancedTypes/AttributesExercise.cs
+++ Exercises/06-AdvancedTypes/AdvancedTypes/ExercisesAdvancedTypes/AttributesExercise.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdvancedTypes.ExercisesAdvancedTypes
+﻿namespace AdvancedTypes.ExercisesAdvancedTypes
 {
     // Set Attribute usage for only properties.
     [AttributeUsage(AttributeTargets.Property)]
