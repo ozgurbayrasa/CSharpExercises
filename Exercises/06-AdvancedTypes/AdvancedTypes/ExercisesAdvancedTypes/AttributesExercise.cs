23e65e94fa8ed96f55074cd40d00b60c75e47065
ozgur bayrasa <bayrasaozgur@gmail.com>
2024-12-29 21:32:49 +0300

    Attributes Exercises Implemented.

---
+++ Exercises/06-AdvancedTypes/AdvancedTypes/ExercisesAdvancedTypes/AttributesExercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTypes.ExercisesAdvancedTypes
+{
+    // Set Attribute usage for only properties.
+    [AttributeUsage(AttributeTargets.Property)]
+    // Attribute class must be inherited.
+    public class MustBeLargerThanAttribute : Attribute
+    {
+        // Only gettable property in attribute.
+        public int Min { get; }
+
+        public MustBeLargerThanAttribute(int min)
+        {
+            Min = min;
+        }
+    }
+
+
+}
