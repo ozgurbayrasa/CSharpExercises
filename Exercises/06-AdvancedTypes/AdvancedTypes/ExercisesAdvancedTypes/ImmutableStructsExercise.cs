d56b1b5094644482c7cfa5d81a9ab65924c8cbaa
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-01-02 00:22:53 +0300

    Immutable 'Time' struct implemented.

--- Exercises/06-AdvancedTypes/AdvancedTypes/ExercisesAdvancedTypes/ImmutableStructsExercise.cs
+++ Exercises/06-AdvancedTypes/AdvancedTypes/ExercisesAdvancedTypes/ImmutableStructsExercise.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace AdvancedTypes.ExercisesAdvancedTypes
 {
-    internal class ImmutableStructsExercise
+    public struct Time
     {
+        public int Hour { get;}
+        public int Minute { get;}
+
+        public Time(int hour, int minute)
+        {
+            if ((hour < 0 || hour > 23) || (minute < 0 || minute > 59))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Hour = hour;
+            Minute = minute;
+        }
+
+        public override string ToString()
+        {
+            return $"{Hour.ToString("00")}:{Minute.ToString("00")}";
+        }
     }
 }
