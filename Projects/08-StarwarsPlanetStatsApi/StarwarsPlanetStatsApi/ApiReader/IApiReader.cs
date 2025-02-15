298ea0433b88d02746b00fef251aa5e913fc0272
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-02-16 00:21:09 +0300

    Api Reader Impelementation

---
+++ Projects/08-StarwarsPlanetStatsApi/StarwarsPlanetStatsApi/ApiReader/IApiReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarwarsPlanetStatsApi.ApiReader
+{
+    internal interface IApiReader
+    {
+        public Task<string> Read(string baseAdress, string requestUri);
+    }
+}
