011d29af72242cd2d7818f67d2f3a8efd5e40528
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-02-15 15:49:49 +0300

    GetHashCode Exercise Implemented

---
+++ Exercises/06-AdvancedTypes/AdvancedTypes/ExercisesAdvancedTypes/GetHashCodeExercise.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTypes.ExercisesAdvancedTypes
+{
+    internal class GetHashCodeExercise
+    {
+        public struct Time
+        {
+            public int Hour { get; }
+            public int Minute { get; }
+
+            public Time(int hour, int minute)
+            {
+                if (hour < 0 || hour > 23)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Hour is out of range of 0-23");
+                }
+                if (minute < 0 || minute > 59)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "Minute is out of range of 0-59");
+                }
+                Hour = hour;
+                Minute = minute;
+            }
+
+            public override string ToString() =>
+                $"{Hour.ToString("00")}:{Minute.ToString("00")}";
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Time otherTime &&
+                       Hour == otherTime.Hour &&
+                       Minute == otherTime.Minute;
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(Hour, Minute);
+            }
+        }
+    }
+}
