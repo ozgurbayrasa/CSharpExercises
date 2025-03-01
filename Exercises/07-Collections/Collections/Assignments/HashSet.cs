0f122d70631caea8459e14cdec2af10c3244e414
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-03-01 21:17:59 +0300

    Hashset Assignment Implemented

---
+++ Exercises/07-Collections/Collections/Assignments/HashSet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Assignments
+{
+    public static class HashSetsUnionExercise
+    {
+        // It should be pure
+        // We should not modify the input sets.
+        public static HashSet<T> CreateUnion<T>(HashSet<T> set1, HashSet<T> set2)
+        {
+            HashSet<T> union = new HashSet<T>(set1); // Copy set1
+            union.UnionWith(set2); // Efficiently add elements from set2
+            return union;
+        }
+    }
+}
