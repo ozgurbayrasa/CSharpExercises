f98bfbb17e8e682f9f9ff79f668d5354a797b2b1
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-03-02 02:24:37 +0300

    Yield and Stack Assignment Impelmented

---
+++ Exercises/07-Collections/Collections/Assignments/StackExtensionParams.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Assignments
+{
+    public static class StackExtensions
+    {
+        // Extension method for Stack<string>
+        // to contain if specific word exists.
+
+        public static bool DoesContainAny(this Stack<string> thisStack, params string[] stringsToCheck)
+        {
+            foreach (string word in stringsToCheck)
+            {
+                if(thisStack.Contains(word))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Exercises/07-Collections/Collections/Assignments/YieldStatement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Assignments
+{
+    public class YieldExercise
+    {
+        public static IEnumerable<T> GetAllAfterLastNullReversed<T>(IList<T> input)
+        {
+           for(int i = input.Count - 1; i >= 0; i--)
+           {
+                if(input[i] is not null)
+                {
+                    yield return input[i];
+                }
+                else
+                {
+                    yield break;
+                }
+           }
+        }
+    }
+}
