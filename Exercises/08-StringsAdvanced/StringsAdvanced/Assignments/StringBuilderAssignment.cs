9171176cf037f877f321c7b8bc4e9c3bd1dd7dc1
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-03-03 17:34:35 +0300

    String Builder Assignment Implemented

---
+++ Exercises/08-StringsAdvanced/StringsAdvanced/Assignments/StringBuilderAssignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAdvanced.Assignments
+{
+    public static class StringBuilderExercise
+    {
+        public static string Reverse(string input)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (char c in input.Reverse())
+            {
+                stringBuilder.Append(c);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
