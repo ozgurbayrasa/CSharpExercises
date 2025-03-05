308db50bc88c12ca6e9b99b601a953c933411e01
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-03-05 11:21:54 +0300

    Checked Number Exercise Implemented

---
+++ Exercises/09-NumbersAdvanced/NumbersAdvanced/Assignments/CheckedFibonacciExercise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersAdvanced.Assignments
+{
+    public static class CheckedFibonacciExercise
+    {
+        public static IEnumerable<int> GetFibonacci(int n)
+        {
+
+            int firstNum = 0;
+            int secondNum = 1;
+
+            if(n > 0) yield return firstNum;
+            if (n > 1) yield return secondNum;
+
+            if (n > 2)
+            {
+                checked
+                {
+                    for (int i = 3; i <= n; i++)
+                    {
+                        int nextNum = firstNum + secondNum;
+                        yield return nextNum;
+                        firstNum = secondNum;
+                        secondNum = nextNum;
+                    }
+
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ Exercises/09-NumbersAdvanced/NumbersAdvanced/Program.cs
@@ -0,0 +1,7 @@
+﻿using NumbersAdvanced.Assignments;
+
+
+foreach (var num in CheckedFibonacciExercise.GetFibonacci(0))
+{
+    Console.WriteLine(num);
+}
