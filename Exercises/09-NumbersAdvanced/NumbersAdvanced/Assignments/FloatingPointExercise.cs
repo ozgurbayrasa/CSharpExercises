504e5be22eeb650b9de3cf6595157666f4663623
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-03-10 19:07:37 +0300

    Events Assignment Implementaiton

---
+++ Exercises/09-NumbersAdvanced/NumbersAdvanced/Assignments/FloatingPointExercise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersAdvanced.Assignments
+{
+    public static class FloatingPointNumbersExercise
+    {
+        private const double tolerance = 0.00001d;
+
+        public static bool IsAverageEqualTo(
+            this IEnumerable<double> input, double valueToBeChecked)
+        {
+            if (input.Any(x => double.IsNaN(x) || double.IsInfinity(x))) throw new ArgumentException();
+
+            return Math.Abs(input.Average() - valueToBeChecked) < tolerance;
+        }
+    }
+}
---
+++ Exercises/10-Events/Events/Assignments/Events.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Assignments
+{
+    public delegate void OnBalanceDecreased(decimal balance);
+
+    public class BankAccount
+    {
+        public decimal Balance { get; private set; }
+        public BankAccount(decimal initialBalance)
+        {
+            Balance = initialBalance;
+        }
+        public event OnBalanceDecreased OnBalanceDecreased;
+
+        public void DecreaseBalance(decimal price)
+        {
+            Balance -= price;
+
+            OnBalanceDecreased(price);
+        }
+
+    }
+
+    public class User
+    {
+        public decimal Funds { get; private set; }
+
+        public User(decimal cash, decimal moneyInBank)
+        {
+            Funds = cash + moneyInBank;
+        }
+
+        public void ReduceFunds(decimal balanceReduced)
+        {
+            Funds -= balanceReduced;
+        }
+    }
+}
