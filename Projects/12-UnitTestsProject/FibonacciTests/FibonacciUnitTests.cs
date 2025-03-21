6763e377cb8981b38d8f3292a44fb382653378f9
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-03-21 19:50:54 +0300

    Implementing Unit Tests for Fibonacci Generator

---
+++ Projects/12-UnitTestsProject/FibonacciTests/FibonacciUnitTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace FibonacciTests
+{
+    [TestFixture]
+    public class FibonacciUnitTests
+    {
+
+        [TestCase(-1)]
+        [TestCase(-2)]
+        public void Generate_ThrowsArgumentException_IfNIsSmallerThanZero(int number)
+        {
+            Assert.That(() => Fibonacci.Generate(number),
+                Throws.ArgumentException);
+        }
+
+        [TestCase(47)]
+        [TestCase(48)]
+        public void Generate_ThrowsArgumentException_IfNIsGreaterThan46(int number)
+        {
+            Assert.That(() => Fibonacci.Generate(47),
+                Throws.ArgumentException);
+        }
+
+        [TestCase(0, new int[] { })]
+        public void Generate_ReturnsEmptyArray_IfInputIsZero(int number, int[] expected)
+        {
+            var result = Fibonacci.Generate(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, new int[] { 0 })]
+        [TestCase(2, new int[] { 0, 1 })]
+        public void Generate_ReturnsExpectedResult_IfInputIsValid(int number, int[] expected)
+        {
+            var result = Fibonacci.Generate(number);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase(46, 1134903170)]
+        public void Generate_LastItemEqualsExpected_IfInputIs46(int number, int expectedLastItem)
+        {
+            var result = Fibonacci.Generate(number);
+            Assert.That(result.Last(), Is.EqualTo(expectedLastItem));
+        }
+    }
+}
---
+++ Projects/12-UnitTestsProject/UnitTestsProject/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine("Hello world!");
+
+// Fibonacci class is defined, then I will generate Unit tests for it.
+public static class Fibonacci
+{
+    public static IEnumerable<int> Generate(int n)
+    {
+        if (n < 0)
+        {
+            throw new ArgumentException(
+                $"{nameof(n)} cannot be smaller than 0.");
+        }
+
+        const int MaxValidNumber = 46;
+        if (n > MaxValidNumber)
+        {
+            throw new ArgumentException(
+                $"{nameof(n)} cannot be larger than {MaxValidNumber}, " +
+                $"as it will cause numeric overflow.");
+        }
+
+        var result = new List<int>();
+
+        int a = -1;
+        int b = 1;
+        for (int i = 0; i < n; i++)
+        {
+            int sum = a + b;
+            result.Add(sum);
+            a = b;
+            b = sum;
+        }
+
+        return result;
+    }
+}
