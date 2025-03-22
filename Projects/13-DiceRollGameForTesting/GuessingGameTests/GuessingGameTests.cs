ff2f3bd4f13c12503b1265ef341f08e90f43ad29
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-03-22 16:02:09 +0300

    Setting Mocks fot Tests

--- Projects/13-DiceRollGameForTesting/GuessingGameTests/GuessingGameTests.cs
+++ Projects/13-DiceRollGameForTesting/GuessingGameTests/GuessingGameTests.cs
@@ -7,5 +7,25 @@ using NUnit.Framework;
 
 namespace GuessingGameTests
 {
-    
+    [TestFixture]
+    public class GuessingGameTests
+    {
+        private Mock<IDice> _diceMock;
+        private Mock<Random> _randomMock;
+        private Mock<IUserCommunication> _userCommunicationMock;
+        private GuessingGame _cut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _randomMock = new Mock<Random>();
+            _diceMock = new Mock<IDice>(_randomMock.Object);
+            _userCommunicationMock = new Mock<IUserCommunication>();
+            _cut = new GuessingGame(_diceMock.Object, _userCommunicationMock.Object);
+        }
+
+
+
+
+    }
 }
