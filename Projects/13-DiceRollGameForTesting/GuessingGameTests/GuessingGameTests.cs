a6ea405c415ec6e7712372f9065e54a2291aa13b
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-03-22 16:28:27 +0300

    Testing when it is correct on First Try.

--- Projects/13-DiceRollGameForTesting/GuessingGameTests/GuessingGameTests.cs
+++ Projects/13-DiceRollGameForTesting/GuessingGameTests/GuessingGameTests.cs
@@ -18,12 +18,34 @@ namespace GuessingGameTests
         [SetUp]
         public void SetUp()
         {
+            // Setting up the mocks
             _randomMock = new Mock<Random>();
-            _diceMock = new Mock<IDice>(_randomMock.Object);
+
+            // Don't forget to pass the mock instance to the constructor.
+            // Never pass the class itself that you are testing to the constructor.
+            _diceMock = new Mock<IDice>();
             _userCommunicationMock = new Mock<IUserCommunication>();
             _cut = new GuessingGame(_diceMock.Object, _userCommunicationMock.Object);
         }
 
+        [Test]
+        public void Play_ReturnsVictory_WhenFirstGuessIsCorrect()
+        {
+            // Arrange
+            _diceMock
+                .Setup(d => d.Roll())
+                .Returns(5);
+
+            _userCommunicationMock
+                .Setup(u => u.ReadInteger(It.IsAny<string>()))
+                .Returns(5);
+
+            // Act
+            var result = _cut.Play();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(GameResult.Victory));
+        }
 
 
 
