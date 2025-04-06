793569e8210d39fe67e0a087fa34d99345360b52
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-04-06 17:25:39 +0300

    Unit Test | Tests for checking range of Min and Max are implemented.

--- Projects/14-RandomPasswordGeneratorRefactoring/RandPasswordGeneratorUnitTests/RandomPasswordGeneratorUnitTests.cs
+++ Projects/14-RandomPasswordGeneratorRefactoring/RandPasswordGeneratorUnitTests/RandomPasswordGeneratorUnitTests.cs
@@ -19,6 +19,38 @@ namespace RandPasswordGeneratorUnitTests
             _cut = new PasswordGenerator(_randomProviderMock.Object);
         }
 
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void Generate_ShallThrowArgumentOutOfRangeException_WhenMinValueLessThan1(int minvalue)
+        {
+            bool isSpecialLetter = false;
+            int maxValue = 5;
+
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => _cut.Generate(minvalue, maxValue, isSpecialLetter));
+        }
+
+        [TestCase(5,4)]
+        [TestCase(-1,-5)]
+        public void Generate_ShallThrowArgumentOutOfRangeException_WhenMinValueGreaterThanMaxValue(int minvalue, int maxValue)
+        {
+            bool isSpecialLetter = false;
+            // Act & Assert
+            Assert.Throws<ArgumentOutOfRangeException>(() => _cut.Generate(minvalue, maxValue, isSpecialLetter));
+        }
+
+        [TestCase(2, 5)]
+        [TestCase(1, 1)]
+        public void Generate_ShallNotThrowException_WhenMinValueGreaterThan0AndLessOrEqualThanMaxValue(int minvalue, int maxValue)
+        {
+            bool isSpecialLetter = false;
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => _cut.Generate(minvalue, maxValue, isSpecialLetter));
+        }
+
+
         
+
     }
 }
