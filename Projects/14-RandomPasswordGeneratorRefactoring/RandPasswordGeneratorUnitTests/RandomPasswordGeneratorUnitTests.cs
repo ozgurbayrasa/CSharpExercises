e061e556ae30885f09dd173678e55e058f3cc8f6
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-04-06 16:53:53 +0300

    Unit Test | Setting up Mocks

--- Projects/14-RandomPasswordGeneratorRefactoring/RandPasswordGeneratorUnitTests/RandomPasswordGeneratorUnitTests.cs
+++ Projects/14-RandomPasswordGeneratorRefactoring/RandPasswordGeneratorUnitTests/RandomPasswordGeneratorUnitTests.cs
@@ -1,7 +1,24 @@
-﻿namespace RandPasswordGeneratorUnitTests
+﻿using Moq;
+using NUnit.Framework;
+
+
+// Since it is a test file, we can safely ignore nullability warnings
+#nullable disable
+namespace RandPasswordGeneratorUnitTests
 {
+    [TestFixture]
     public class RandomPasswordGeneratorUnitTests
     {
+        private PasswordGenerator _cut; // Class under test
+        private Mock<IRandomProvider> _randomProviderMock;
 
+        [SetUp]
+        public void SetUp()
+        {
+            _randomProviderMock = new Mock<IRandomProvider>();
+            _cut = new PasswordGenerator(_randomProviderMock.Object);
+        }
+
+        
     }
 }
