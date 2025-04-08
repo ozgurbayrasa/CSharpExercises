629e4be2e74be3987ed19b0d623bedd62bb94fa2
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-04-09 01:16:01 +0300

    Threads Execise | Creating Threads

---
+++ Exercises/14-MultithreadingAndAsynchrony/MultithreadingAndAsynchrony/Assigments/CreatingThreads.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingAndAsynchrony.Assigments
+{
+    internal class CreatingThreads
+    {
+        public static void RunThreads()
+        {
+            Thread thread1 = new Thread(ThreadMethod);
+            Thread thread2 = new Thread(ThreadMethod);
+            Thread thread3 = new Thread(ThreadMethod);
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+        private static void ThreadMethod()
+        {
+            Console.WriteLine($"Hello from thread with ID: {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
---
+++ Exercises/14-MultithreadingAndAsynchrony/MultithreadingAndAsynchrony/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MultithreadingAndAsynchrony.Assigments;
+
+Console.WriteLine("Hello, World!");
+CreatingThreads.RunThreads();
