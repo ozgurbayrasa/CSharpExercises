168d8814ecca84128a7b3b739653429a223a8e9b
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-04-09 10:32:47 +0300

    Exercise Tasks | Task Waiting

---
+++ Exercises/14-MultithreadingAndAsynchrony/MultithreadingAndAsynchrony/Assigments/TasksWaiting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingAndAsynchrony.Assigments
+{
+    internal class TasksWaiting
+    {
+        public static void RunSimpleTask()
+        {
+            Console.WriteLine("Before task starting.");
+
+            var task1 = Task.Run(() =>
+            {
+
+                for(int i = 1; i < 4; i++)
+                {
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine($"Iteration number {i}");
+                }
+
+            });
+
+            task1.Wait();
+
+            Console.WriteLine("The task has finished.");
+        }
+    }
+}
