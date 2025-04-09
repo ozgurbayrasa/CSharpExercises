a7883b86e5c6617eaa9861d4d53932fcf1286834
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-04-09 11:26:07 +0300

    Tasks Exercise | ContinueWith On Tasks

---
+++ Exercises/14-MultithreadingAndAsynchrony/MultithreadingAndAsynchrony/Assigments/TaskContinue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingAndAsynchrony.Assigments
+{
+    internal class TaskContinue
+    {
+        public static Task<string> FormatSquaredNumbersFrom1To(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("The number must be greater than 0.");
+            }
+            return Task.Run(() => SquareNumbers(n))
+                .ContinueWith(SquareNumbers =>
+                {
+                    var result = string.Join(", ", SquareNumbers.Result);
+                    return result;
+                });
+        }
+
+        private static List<int> SquareNumbers(int n)
+        {
+            
+
+            var squaredNumbers = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                squaredNumbers.Add(i * i);
+            }
+
+            return squaredNumbers;
+        }
+
+        
+        
+    }
+}
