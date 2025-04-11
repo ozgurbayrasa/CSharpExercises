8e971bdba18fcc9488e7ee0e9230dba09d975183
ozgur bayrasa <bayrasaozgur@gmail.com>
2025-04-12 01:57:15 +0300

    Task Exercise | Async Method Exercise

---
+++ Exercises/14-MultithreadingAndAsynchrony/MultithreadingAndAsynchrony/Assigments/AwaitAsync.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultithreadingAndAsynchrony.Assigments
+{
+    internal class AwaitAsync
+    {
+        public async void Test()
+        {
+            string data = await DownloadDataAsync("test.com", "some content");
+            Console.WriteLine(data);
+        }
+
+        public async Task<string> DownloadDataAsync(string Url, string content)
+        {
+            await Task.Delay(1000);
+            return $"Content from URL '{Url}' is '{content}'";
+        }
+    }
+}
